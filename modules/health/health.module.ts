4589bfc97c76effbda246519e4b7a334bb6da125
Raymond Tam <raymondtamtwtp@gmail.com>
2024-04-25 09:59:27 +0800

    e

--- modules/health/health.module.ts
+++ modules/health/health.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";\
+import { Module } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { HealthController } from "./health.controller";
 
